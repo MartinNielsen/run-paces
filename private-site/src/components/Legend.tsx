38857458a64242f37a36faab336f53511f9c179e
Martin D. Nielsen <martin@plug-n-trade.com>
2025-07-13 17:31:19 +0200

    New version

--- private-site/src/components/Legend.tsx
+++ private-site/src/components/Legend.tsx
@@ -4,15 +4,18 @@ const Legend = () => {
       position: 'absolute',
       top: '10px',
       right: '10px',
-      backgroundColor: 'white',
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
       padding: '10px',
       borderRadius: '5px',
-      zIndex: 1000
+      zIndex: 1000,
+      textAlign: 'left'
     }}>
       <h4>Activity Legend</h4>
       <div><span style={{color: 'red'}}>■</span> Run</div>
       <div><span style={{color: 'green'}}>■</span> Hike</div>
       <div><span style={{color: 'blue'}}>■</span> Cycle</div>
+      <div><span style={{color: 'purple'}}>■</span> Walking</div>
+      <div><span style={{color: 'brown'}}>■</span> Rucking</div>
     </div>
   );
 };
